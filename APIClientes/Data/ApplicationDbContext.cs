38ebb548a50d07b65fd4465428cd564e5a9ab7af
jerickvasquez <jorgeerick446@gmail.com>
2022-03-23 20:20:17 -0500

    Creacion de la carpeta Data

---
+++ APIClientes/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using APIClientes.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIClientes.Data
+{
+    //Se hereda de la clase DbContext
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base (options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
